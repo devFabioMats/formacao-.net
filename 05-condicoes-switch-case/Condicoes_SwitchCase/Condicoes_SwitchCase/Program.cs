996cb33ea68466219c0b2e88fccbff5e1c4d8df7
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-11-29 14:56:38 -0300

    Aprendendo switch case

--- 05-condicoes-switch-case/Condicoes_SwitchCase/Condicoes_SwitchCase/Program.cs
+++ 05-condicoes-switch-case/Condicoes_SwitchCase/Condicoes_SwitchCase/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Condicoes_SwitchCase
 {
@@ -10,6 +6,32 @@ namespace Condicoes_SwitchCase
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Escola uma das opções");
+            Console.WriteLine("1 - criar arquivo");
+            Console.WriteLine("2 - abrir arquivo");
+            Console.WriteLine("3 - salvar arquivo");
+            Console.WriteLine("0 - sair");
+
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Opção 1 selecionado");
+                    break;
+                case 2:
+                    Console.WriteLine("Opção 2 selecionado");
+                    break;
+                case 3:
+                    Console.WriteLine("Opção 3 selecionado");
+                    break;
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+
         }
     }
 }
