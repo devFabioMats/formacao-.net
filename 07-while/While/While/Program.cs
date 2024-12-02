69ac275b3a29f89189934193f7b15cd8c109c9a2
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 09:58:29 -0300

    while

---
+++ 07-while/While/While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("---------------------------");
+
+            int j = 0;
+            while (j < 10)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+        }
+    }
+}
