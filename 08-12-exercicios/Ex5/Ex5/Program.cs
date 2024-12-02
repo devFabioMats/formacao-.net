e40c46d475b478b3d261564805b85b9852117f70
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 10:43:41 -0300

    ex5

---
+++ 08-12-exercicios/Ex5/Ex5/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Enunciado: Calcular troco
+*/
+
+using System;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float valorPago = 0,
+                valorProduto = 0,
+                troco = 0;
+
+
+            Console.WriteLine("Informe o valor pago: ");
+            valorPago = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor do produto: ");
+            valorProduto = float.Parse(Console.ReadLine());
+
+            troco = valorPago - valorProduto;
+
+            Console.WriteLine("O seu troco será de R$ " + troco.ToString("F2"));
+        }
+    }
+}
