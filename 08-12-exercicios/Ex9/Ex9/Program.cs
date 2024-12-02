91aee60d22b53928aa78e22481674cbe82516276
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 11:39:40 -0300

    ex9

---
+++ 08-12-exercicios/Ex9/Ex9/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Enunciado: Verifique se o numero é impar
+*/
+
+using System;
+
+namespace Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("Digite um número para verificar se ele é impar: ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("O número " + n + " é par.");
+            }
+            else
+            {
+                Console.WriteLine("O número " + n + " é ímpar.");
+            }
+        }
+    }
+}
