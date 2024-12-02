b25dc27b733a8f55c45a0e59cdc1aec1874405f6
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 11:50:29 -0300

    ex10

---
+++ 08-12-exercicios/Ex10/Ex10/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Enunciado: Receber os lados do triangulo e dizer se eles são equiláteros, isósceles ou escaleno.
+*/
+
+using System;
+
+namespace Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l1 = 0,
+                l2 = 0,
+                l3 = 0;
+
+            Console.WriteLine("Digite o medida do lado 1 do triângulo: ");
+            l1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o medida do lado 2 do triângulo: ");
+            l2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o medida do lado 3 do triângulo: ");
+            l3 = int.Parse(Console.ReadLine());
+
+            if (l1 == l2 && l1 == l3 && l2 == l3)
+            {
+                Console.WriteLine("O triângulo é equilátero!");
+            }
+            else if (l1 == l2 || l1 == l3 || l2 == l3)
+            {
+                Console.WriteLine("O triângulo é isósceles!");
+            }
+            else
+            {
+                Console.WriteLine("O triângulo é escaleno!");
+            }
+        }
+    }
+}
