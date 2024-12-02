db230178b19eeccedd925af4898a0dd64cbac33d
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 10:36:05 -0300

    ex4

---
+++ 08-12-exercicios/Ex4/Ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Enunciado: Calcular área do triângulo
+*/
+
+using System;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseTriangulo = 0,
+                alturaTriangulo = 0,
+                area = 0;
+
+            Console.WriteLine("Informe a base do triângulo: ");
+            baseTriangulo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura do triângulo: ");
+            alturaTriangulo = int.Parse(Console.ReadLine());
+
+            area = baseTriangulo * alturaTriangulo / 2;
+
+            Console.WriteLine("A área do triângulo informado é: " + area);
+        }
+    }
+}
