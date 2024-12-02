3e0ac550f741aadbf0d1b7dd63fdb33f6ebb84aa
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 11:02:44 -0300

    ex7

---
+++ 08-12-exercicios/Ex6/Ex6/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Enunciado: Calcular preço pelo valor do quilo e a quantidade de quilos
+*/
+
+using System;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float valorQuilo = 0,
+                  quantidadeProduto = 0,
+                  valorFinal = 0;
+
+            Console.WriteLine("Informe o valor do quilo do produto: ");
+            valorQuilo = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade de produto: ");
+            quantidadeProduto = float.Parse(Console.ReadLine());
+
+            valorFinal = valorQuilo * quantidadeProduto;
+
+            Console.WriteLine("O valor final será de R$ " + valorFinal.ToString("F2"));
+        }
+    }
+}
---
+++ 08-12-exercicios/Ex7/Ex7/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Enunciado: receber dois valores e retornar o maior
+*/
+
+using System;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 0,
+                valor2 = 0;
+
+            Console.WriteLine("Valor 1: ");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor 2: ");
+            valor2 = int.Parse(Console.ReadLine());
+
+            if (valor1 > valor2)
+            {
+                Console.WriteLine("O maior valor é: " + valor1);
+            }
+            else
+            {
+                Console.WriteLine("O maior valor é: " + valor2);
+            }
+        }
+    }
+}
