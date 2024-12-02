28d95e7d316c7204a28c54b7140d0fd949111c23
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 14:34:13 -0300

    exercicios

--- 08-12-exercicios/Ex15/Ex15/Properties/AssemblyInfo.cs
+++ 08-12-exercicios/Ex15/Ex15/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("Ex14")]
+[assembly: AssemblyTitle("Ex15")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ex14")]
+[assembly: AssemblyProduct("Ex15")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,12 +20,12 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("a63a844d-0952-4117-b938-bcaaa1777408")]
+[assembly: Guid("cb04e900-f2ce-43e9-a9fb-515bf130e1b0")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
 //      Versão Principal
-//      Versão Secundária
+//      Versão Secundária 
 //      Número da Versão
 //      Revisão
 //
