e2f3740ac99119897cb83d70839f030f5029c423
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 10:29:23 -0300

    ex3

---
+++ 08-12-exercicios/Ex3/Ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Enunciado: Crie um algoritmo que após ler dois números inteiros, apresenta a sua diferença.
+*/
+
+using System;
+
+namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0,
+                n2 = 0,
+                dif = 0;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            dif = n1 - n2;
+
+            Console.WriteLine("A diferença entre os números é: " + dif);
+
+
+
+        }
+    }
+}
