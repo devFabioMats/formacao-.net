44d141b94bf59bc6d401c63b32987554a87ac76c
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 10:05:55 -0300

    ex1

---
+++ 08-12-exercicios/Ex1/Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Enunciado: Crie um algoritmo que lê o nome de uma pessoa e escreve "Olá" segido do nome da pessoa.
+*/
+
+using System;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Olá, " + nome);
+        }
+    }
+}
