7586fac4a4dd1244127aed7b758d505dff127c9c
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-02 11:35:50 -0300

    ex8

---
+++ 08-12-exercicios/Ex8/Ex8/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Enunciado: receber quatro valores e retornar o menor
+*/
+
+using System;
+
+namespace Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 0,
+                valor2 = 0,
+                valor3 = 0,
+                valor4 = 0;
+
+            Console.WriteLine("Valor 1: ");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor 2: ");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor 3: ");
+            valor3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor 4: ");
+            valor4 = int.Parse(Console.ReadLine());
+
+            if (valor1 < valor2 && valor1 < valor3 && valor1 < valor4)
+            {
+                Console.WriteLine("O menor valor é: " + valor1);
+            }
+            else if (valor2 < valor1 && valor2 < valor3 && valor2 < valor4)
+            {
+                Console.WriteLine("O menor valor é: " + valor2);
+            }
+            else if (valor3 < valor1 && valor3 < valor2 && valor3 < valor4)
+            {
+                Console.WriteLine("O menor valor é: " + valor3);
+            }
+            else
+            {
+                Console.WriteLine("O menor valor é: " + valor4);
+            }
+        }
+    }
+}
