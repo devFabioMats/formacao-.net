582854d428582aafdcea3252725263134131df1a
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-03 11:33:50 -0300

    projeto concluido

--- 15-projeto-casino/ProjetoLoteria/ProjetoLoteria/Program.cs
+++ 15-projeto-casino/ProjetoLoteria/ProjetoLoteria/Program.cs
@@ -78,6 +78,7 @@ namespace ProjetoLoteria
 
             while (op != 0)
             {
+                Console.Clear();
                 Console.WriteLine("Menu Raspadinhas:");
                 Console.WriteLine("1 - Raspadinha A");
                 Console.WriteLine("2 - Raspadinha B");
@@ -104,11 +105,77 @@ namespace ProjetoLoteria
 
         private static void RaspadinhaA()
         {
-            throw new NotImplementedException();
+            Console.Clear();
+
+            int numeroPrincipal = rnd.Next(1, 10);
+            int n1 = rnd.Next(1, 10); int p1 = rnd.Next(1, 100);
+            int n2 = rnd.Next(1, 10); int p2 = rnd.Next(1, 100);
+            int n3 = rnd.Next(1, 10); int p3 = rnd.Next(1, 100); 
+            int n4 = rnd.Next(1, 10); int p4 = rnd.Next(1, 100);
+            int n5 = rnd.Next(1, 10); int p5 = rnd.Next(1, 100);
+
+            Console.WriteLine("Número principal: " + numeroPrincipal);
+            Console.WriteLine("1º número " + n1 + " (R$ " + p1 + ")");
+            Console.WriteLine("2º número " + n2 + " (R$ " + p2 + ")");
+            Console.WriteLine("3º número " + n3 + " (R$ " + p3 + ")");
+            Console.WriteLine("4º número " + n4 + " (R$ " + p4 + ")");
+            Console.WriteLine("5º número " + n5 + " (R$ " + p5 + ")");
+
+            int premio = 0;
+
+            if (numeroPrincipal == n1) premio += p1;
+            if (numeroPrincipal == n2) premio += p2;
+            if (numeroPrincipal == n3) premio += p3;
+            if (numeroPrincipal == n4) premio += p4;
+            if (numeroPrincipal == n5) premio += p5;
+
+            Console.WriteLine("Prêmio: R$ " + premio);
+
+            Console.ReadKey();
         }
         private static void RaspadinhaB()
         {
+            Console.Clear();
 
+            int numeroPrincipal = rnd.Next(1, 10);
+            int premio = rnd.Next(1, 100);
+
+            int n1 = rnd.Next(1, 10);
+            int n2 = rnd.Next(1, 10);
+            int n3 = rnd.Next(1, 10);
+            int n4 = rnd.Next(1, 10);
+            int n5 = rnd.Next(1, 10);
+            int n6 = rnd.Next(1, 10);
+            int n7 = rnd.Next(1, 10);
+            int n8 = rnd.Next(1, 10);
+            int n9 = rnd.Next(1, 10);
+
+            int contador = 0;
+
+            if (numeroPrincipal == n1) contador++;
+            if (numeroPrincipal == n2) contador++;
+            if (numeroPrincipal == n3) contador++;
+            if (numeroPrincipal == n4) contador++;
+            if (numeroPrincipal == n5) contador++;
+            if (numeroPrincipal == n6) contador++;
+            if (numeroPrincipal == n7) contador++;
+            if (numeroPrincipal == n8) contador++;
+            if (numeroPrincipal == n9) contador++;
+
+            Console.WriteLine("Número principal: " + numeroPrincipal + " (Prêmio: R$ " + premio + ")");
+            Console.WriteLine(n1 + " " + n2 + " " + n3);
+            Console.WriteLine(n4 + " " + n5 + " " + n6);
+            Console.WriteLine(n7 + " " + n8 + " " + n9);
+
+            if (contador >= 3) {
+                Console.WriteLine("Parabéns! Você foi premiado! (Ganhou: R$ " + premio + ")");
+                premio += 100;
+            } else
+            {
+                Console.WriteLine("Que pena! Raspadinha não premiada!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
