d7433dd5183093c80c6a902e76354f58d2837a19
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-03 13:43:35 -0300

    arrays

--- 19-arrays/Arrays/Arrays/Program.cs
+++ 19-arrays/Arrays/Arrays/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Arrays
 {
@@ -10,6 +6,33 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            string nome1 = "Ana Silva";
+            string nome2 = "Maria Gomes";
+            string nome3 = "Vander Lobão";
+            string nome4 = "José Valdo";
+            string nome5 = "Main Yasuo";
+
+            Console.WriteLine("Nome 1: " + nome1);
+            Console.WriteLine("Nome 2: " + nome2);
+            Console.WriteLine("Nome 3: " + nome3);
+            Console.WriteLine("Nome 4: " + nome4);
+            Console.WriteLine("Nome 5: " + nome5);
+
+            Console.WriteLine("--------------------------------------------------------");
+
+            string[] nomes = new string[] { "Ana", "Maria", "Vander", "José", "Main" };
+
+            string[] sobrenomes = new string[5];
+            sobrenomes[0] = "Silva";
+            sobrenomes[1] = "Gomes";
+            sobrenomes[2] = "Lobão";
+            sobrenomes[3] = "Valdo";
+            sobrenomes[4] = "Yasuo";
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("Nome " + (i + 1) + ": " + nomes[i] + " " + sobrenomes[i]);
+            }
         }
     }
 }
