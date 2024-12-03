67a43507c436e8f42fde8df92d91cbba817072d4
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-03 14:56:36 -0300

    Update Program.cs

--- 21-22-arrays-exercicios/Ficha1-Ex1/Ficha1-Ex1/Program.cs
+++ 21-22-arrays-exercicios/Ficha1-Ex1/Ficha1-Ex1/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ficha1_Ex1
+﻿namespace Ficha1_Ex1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
