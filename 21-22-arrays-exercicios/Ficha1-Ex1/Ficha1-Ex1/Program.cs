f647f00cc2cd80e884a73831ba10459af6920d61
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-04 08:39:07 -0300

    implemetnando o vetor

--- 21-22-arrays-exercicios/Ficha1-Ex1/Ficha1-Ex1/Program.cs
+++ 21-22-arrays-exercicios/Ficha1-Ex1/Ficha1-Ex1/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            double[] notas = new double[2];
 
+            for (int i = 0; i < notas.Length; i++)
+            {
+
+            }
         }
     }
 }
