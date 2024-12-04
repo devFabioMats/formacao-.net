f077fc18adb72e0dfa2708c09b1d7d2ff9a29636
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-04 08:53:53 -0300

    ex1 feito

--- 21-22-arrays-exercicios/Ficha1-Ex1/Ficha1-Ex1/Program.cs
+++ 21-22-arrays-exercicios/Ficha1-Ex1/Ficha1-Ex1/Program.cs
@@ -1,15 +1,28 @@
-﻿namespace Ficha1_Ex1
+﻿// Façça um programa que peça ao utilizador para introduzir três notas e calcule a média das notas.
+
+using System;
+
+namespace Ficha1_Ex1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double[] notas = new double[2];
+            double soma = 0,
+                   media = 0;
+            double[] notas = new double[3];
 
             for (int i = 0; i < notas.Length; i++)
             {
+                Console.WriteLine("Digite a " + (i + 1) + "º nota: ");
+                notas[i] = double.Parse(Console.ReadLine());
 
+                soma = soma + notas[i];
             }
+
+            media = soma / notas.Length;
+
+            Console.WriteLine("A média das notas é: " + media);
         }
     }
 }
