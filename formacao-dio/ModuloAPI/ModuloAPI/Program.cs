9e26003bdb2b1a7155b59231ba9e9d4b76f2e912
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-09 09:24:04 -0300

    add conexao ao bd

--- formacao-dio/ModuloAPI/ModuloAPI/Program.cs
+++ formacao-dio/ModuloAPI/ModuloAPI/Program.cs
@@ -1,6 +1,11 @@
+using ModuloAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>     // Adiconar um dbcontext do tipo agendacontext e passando algumas opções
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConexaoPadrao")));    // agendacontext, use o postgres e pegue a configuração do appsettings, e pegue a chave conexaopadrao
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
