6298d4d0204c09011574233b8712c60569db5ddf
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-10 12:00:47 -0300

    colocando o criar buscar e updae

--- formacao-dio/ModuloAPI/ModuloAPI/Controllers/ContatoController.cs
+++ formacao-dio/ModuloAPI/ModuloAPI/Controllers/ContatoController.cs
@@ -15,12 +15,41 @@ namespace ModuloAPI.Controllers
             _context = context;
         }
 
-        [HttpPost("CriarContato")]
+        [HttpPost]
         public IActionResult Create(Contato contato)
         {
             _context.Add(contato);
             _context.SaveChanges();
             return Ok(contato);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id); // .Contato vem do DbContext
+
+            if (contato == null)
+                return NotFound();
+            else
+                return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)     // chama o id do contato a ser modificado, chama o json dos dados do contatos a serem modificados
+        {
+            var contatoBanco = _context.Contatos.Find(id);      // busca o contato no banco de dados
+
+            if (contatoBanco == null)       // se o contato não existir, retorna NotFound
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;       // dados do banco recebe dados que vem da requisição
+            contatoBanco.Telefone = contato.Telefone;       // dados do banco recebe dados que vem da requisição
+            contatoBanco.Ativo = contato.Ativo;     // dados do banco recebe dados que vem da requisição
+
+            _context.Contatos.Update(contatoBanco);     // atualiza o contato no banco de dados
+            _context.SaveChanges();     // salva as alterações
+
+            return Ok(contatoBanco);    // retorna o contato atualizado
         }
     }
 }
